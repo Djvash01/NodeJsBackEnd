26261006d70ee355cc1eccbf0f9a3956cb6ff47e
David Yepes <djvash01@gmail.com>
2019-04-26 18:57:08 -0500

    Mi primer  commit

---
+++ src/database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const URI = "mongodb://localhost/mern-tasks";
+mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    })
+    .then(db => console.log("DB is connected"))
+    .catch(err => console.error(err));
+
+module.exports = mongoose;
